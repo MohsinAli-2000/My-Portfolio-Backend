2b7c0a3905ee1cc2bd0183f55f86dfef3dd59379
Mohsin Ali <141604446+MohsinAli-2000@users.noreply.github.com>
2024-06-11 10:11:21 +0500

    Update app.js
    app.js file updated add headers explicitly

--- app.js
+++ app.js
@@ -12,27 +12,48 @@ import softwareRouter from "./routes/software.route.js";
 import skillRouter from "./routes/skill.route.js";
 import projectRouter from "./routes/project.route.js";
 import certificateRouter from "./routes/certificate.route.js";
-//configuring dotenv file
+
+// Configuring dotenv file
 dotenv.config({
   path: "./config/config.env",
 });
+
 const app = express();
 
-//configuring cross origin sharing
+// Configuring cross origin sharing
+const allowedOrigins = [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL];
 app.use(
   cors({
-    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
 
+// Middleware to set headers for every response
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
-//configuring cookie-parser middleware
+// Configuring cookie-parser middleware
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//configuring fileupload
+
+// Configuring file upload
 app.use(
   fileUpload({
     useTempFiles: true,
@@ -40,7 +61,7 @@ app.use(
   })
 );
 
-//message router
+// Routes
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/timeline", timelineRouter);
@@ -48,6 +69,11 @@ app.use("/api/v1/softwares", softwareRouter);
 app.use("/api/v1/skills", skillRouter);
 app.use("/api/v1/projects", projectRouter);
 app.use("/api/v1/certificates", certificateRouter);
+
+// Database connection
 dbConnection();
+
+// Error middleware
 app.use(errorMiddleware);
+
 export default app;
